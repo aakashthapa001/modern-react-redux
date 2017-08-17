f473f4979567bc2040e5f78daab1ebd513ea6c34
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2017-08-17 14:53:01 +0545

    Books listing app with react and redux setup

---
+++ book-list-app-with-redux/README.md
@@ -0,0 +1,3 @@
+# Books list APP with React and Redux
+
+A simple books listing APP with React and Redux.
---
+++ book-list-app-with-redux/src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
