093428dbc0aeec5e9d32943134a3800ca3cfe4b4
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2017-08-17 15:02:46 +0545

    Basic reducer for book added

---
+++ book-list-app-with-redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'You Dont\'t Know JS' },
+    { title: 'Eloquent JS' },
+    { title: 'JS: The Good Parts' },
+    { title: 'Fullstack React' }
+  ]
+}
