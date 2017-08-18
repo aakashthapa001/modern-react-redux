79b4c29f385e223ce5cc3a50a2f91ef4063a5c5b
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2017-08-18 12:06:18 +0545

    Created Booklist container maped with state watched by redux

--- book-list-app-with-redux/src/reducers/reducer_books.js
+++ book-list-app-with-redux/src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function() {
   return [
-    { title: 'You Dont\'t Know JS' },
+    { title: 'You Don\'t Know JS' },
     { title: 'Eloquent JS' },
     { title: 'JS: The Good Parts' },
     { title: 'Fullstack React' }
