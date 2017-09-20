426ebddc497f73596e3e4a8a5ce0e96354684890
Aakash Thapa <thapa.aakash100@gmail.com>
2017-09-20 07:30:52 +0545

    Deleted previous book list project and added new one

--- book-list-app-with-redux/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { title: 'You Don\'t Know JS' },
-    { title: 'Eloquent JS' },
-    { title: 'JS: The Good Parts' },
-    { title: 'Fullstack React' }
-  ]
-}
